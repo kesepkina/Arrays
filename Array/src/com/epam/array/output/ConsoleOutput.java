76a47cddc7dc15aed376579fdce4c417d46905df
Kseniya <43503787+kesepkina@users.noreply.github.com>
2020-11-26 00:37:04 +0300

    Add files via upload

--- Array/src/com/epam/array/output/ConsoleOutput.java
+++ Array/src/com/epam/array/output/ConsoleOutput.java
@@ -1,6 +1,7 @@
 package com.epam.array.output;
 
 import com.epam.array.entity.Array;
+import com.epam.array.entity.JaggedArray;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,5 +17,16 @@ public class ConsoleOutput {
 
     public void printArray(String message, Array array) {
         logger.log(Level.INFO, message + " " + Arrays.toString(array.getValues()));
+    }
+
+    public void printJaggedArray(String message, JaggedArray jaggedArray) {
+        int[][] values = jaggedArray.getValues();
+
+        StringBuilder output = new StringBuilder();
+        for (int[] array : values){
+            output.append(Arrays.toString(array)).append("\n");
+        }
+
+        logger.log(Level.INFO, message + "\n" + output);
     }
 }
